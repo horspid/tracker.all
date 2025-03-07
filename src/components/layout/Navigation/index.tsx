aa37f5d9e4e7a5545bf730d30e7d806ceb877219
Maksim Gudovshchikov <horspid@yandex.ru>
2025-03-07 18:42:58 +0500

    added active status

--- src/components/layout/Navigation/index.tsx
+++ src/components/layout/Navigation/index.tsx
@@ -5,41 +5,66 @@ import CalendarICO from "@assets/images/icons/calendar.svg?react";
 import CategoryICO from "@assets/images/icons/category.svg?react";
 import FavoriteICO from "@assets/images/icons/favorite.svg?react";
 import RatingICO from "@assets/images/icons/rating.svg?react";
-import { Link } from "react-router";
+import { NavLink } from "react-router";
 
 const Navigation = () => {
   return (
     <nav className={styles.menu}>
       <ul>
         <li className={styles.menu__item}>
-          <Link to="/">
-            <BrowseICO />
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? styles.menu__item_active : ""
+            }
+          >
+            <BrowseICO className={styles.ico__browse} />
             <span>Browse</span>
-          </Link>
+          </NavLink>
         </li>
         <li className={styles.menu__item}>
-          <Link to="/categories">
-            <CategoryICO />
+          <NavLink
+            to="/categories"
+            className={({ isActive }) =>
+              isActive ? styles.menu__item_active : ""
+            }
+          >
+            <CategoryICO className={styles.ico__category} />
             <span>Categories</span>
-          </Link>
+          </NavLink>
         </li>
         <li className={styles.menu__item}>
-          <Link to="/watchlist">
-            <FavoriteICO />
+          <NavLink
+            to="/watchlist"
+            className={({ isActive }) =>
+              isActive ? styles.menu__item_active : ""
+            }
+          >
+            <FavoriteICO className={styles.ico__favorite} />
             <span>Watchlist</span>
-          </Link>
+          </NavLink>
         </li>
         <li className={styles.menu__item}>
-          <Link to="/ratings">
-            <RatingICO />
+          <NavLink
+            to="/ratings"
+            className={({ isActive }) =>
+              isActive ? styles.menu__item_active : ""
+            }
+          >
+            <RatingICO className={styles.ico__rating} />
             <span>My Ratings</span>
-          </Link>
+          </NavLink>
         </li>
         <li className={styles.menu__item}>
-          <Link to="/soon">
-            <CalendarICO />
+          <NavLink
+            to="/soon"
+            className={({ isActive }) =>
+              isActive ? styles.menu__item_active : ""
+            }
+          >
+            <CalendarICO className={styles.ico__calendar} />
             <span>Coming soon</span>
-          </Link>
+          </NavLink>
         </li>
       </ul>
     </nav>
