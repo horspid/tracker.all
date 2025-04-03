f5534a998d9eb76e022cec6f93373e4c036bb0a3
Maksim Gudovshchikov <horspid@yandex.ru>
2025-04-03 15:50:51 +0500

    updated for vercel

--- src/routes/Browse/index.tsx
+++ src/routes/Browse/index.tsx
@@ -3,15 +3,15 @@ import Movie from "@assets/images/icons/movie.svg?react";
 import ProductCard from "@components/ui/ProductCard";
 import { useEffect, useState } from "react";
 
-import { useMovieStore } from "../../store/popularFilmes.ts";
+import { useMovieStore } from "@store/popularFilmes.ts";
 
 const Browse = () => {
   const { data, getData } = useMovieStore();
-  const [popularDate, setPopularDate] = useState<string>('day');
+  const [popularDate, setPopularDate] = useState<string>("day");
 
   const popularChangeHandler = () => {
-    setPopularDate(popularDate === 'day' ? 'week' : 'day')  
-  }
+    setPopularDate(popularDate === "day" ? "week" : "day");
+  };
 
   useEffect(() => {
     getData();
@@ -22,15 +22,15 @@ const Browse = () => {
       <div className={styles.browse__content}>
         <div className={styles.browse__entry}>
           <Movie />
-          <h1 className={styles.browse__title}>Popular in this <span onClick={popularChangeHandler}>{popularDate}</span></h1>
+          <h1 className={styles.browse__title}>
+            Popular in this{" "}
+            <span onClick={popularChangeHandler}>{popularDate}</span>
+          </h1>
         </div>
       </div>
       <div className={styles.browse__items}>
         {data.map((item) => (
-          <ProductCard
-            key={item.id}
-            data={item}
-          />
+          <ProductCard key={item.id} data={item} />
         ))}
       </div>
     </section>
