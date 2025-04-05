dd7c9ea6902ffe99c973c75e389ab15791201e7a
Maksim Gudovshchikov <horspid@yandex.ru>
2025-04-05 23:56:46 +0500

    hotfix unlog user ratings

--- src/services/userRatings.ts
+++ src/services/userRatings.ts
@@ -2,52 +2,60 @@ import { supabase } from "@config/database";
 import { useUserStore } from "@store/userStore";
 import { fetchMovieFromIds } from "./userFavorites";
 
-export const insertRatedMovie = async (rating: number, movieId: number ) => {
-    const { data: userData, error: userError } = await supabase.auth.getUser();
+export const insertRatedMovie = async (rating: number, movieId: number) => {
+  const { data: userData, error: userError } = await supabase.auth.getUser();
 
-    if (userError || !userData.user) {
-        throw new Error("Пользователь не авторизован");
-    }
-  
-    const { error } = await supabase
-      .from("ratings")
-      .upsert({ movie_id: movieId, user_id: userData.user.id, user_rating: rating }, { onConflict: 'user_id, movie_id' });
-  
-    if (error) {
-      throw new Error(`Ошибка при добавлении в список: ${error.message}`);
-    }
+  if (userError || !userData.user) {
+    throw new Error("Пользователь не авторизован");
+  }
+
+  const { error } = await supabase
+    .from("ratings")
+    .upsert(
+      { movie_id: movieId, user_id: userData.user.id, user_rating: rating },
+      { onConflict: "user_id, movie_id" },
+    );
+
+  if (error) {
+    throw new Error(`Ошибка при добавлении в список: ${error.message}`);
+  }
 };
 
 export const fetchUserRatings = async () => {
+  try {
     const { data: userData, error: userError } = await supabase.auth.getUser();
     const { setUserRatings } = useUserStore.getState();
 
     if (userError || !userData.user) {
-        throw new Error("Пользователь не авторизован");
+      setUserRatings([]);
+      return [];
     }
 
     const { data, error } = await supabase
-        .from("ratings")
-        .select("*")
-        .eq("user_id", userData.user.id)
+      .from("ratings")
+      .select("*")
+      .eq("user_id", userData.user.id);
 
     if (error) {
-        console.error("Ошибка при загрузке рейтингов:", error);
-        throw new Error("Ошибка при загрузке рейтингов");
+      console.error("Ошибка при загрузке рейтингов:", error);
+      setUserRatings([]);
+      return [];
     }
-    
+
     if (!data || data.length === 0) {
-        console.log("У пользователя нет рейтингов.");
-        return setUserRatings([]);
+      setUserRatings([]);
+      return [];
     }
 
     if (data) {
-        setUserRatings(data);
-        
-        const result = data.map((item) => item.movie_id)
-      
-        const fetchedMovies = await fetchMovieFromIds(result)
-        return fetchedMovies;
-  
+      setUserRatings(data);
+      const result = data.map((item) => item.movie_id);
+      const fetchedMovies = await fetchMovieFromIds(result);
+
+      return fetchedMovies;
     } else return [];
-}
+  } catch (error) {
+    console.error("Ошибка в чтении оценённых фильмов", error);
+    return [];
+  }
+};
