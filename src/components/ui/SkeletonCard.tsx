2c63901107951b81d2b6a0d838a11ded288a39ab
Maksim <horspid@yandex.ru>
2025-05-04 13:13:30 +0500

    updated skeletons

--- src/components/ui/SkeletonCard.tsx
+++ src/components/ui/SkeletonCard.tsx
@@ -8,16 +8,16 @@ const SkeletonCard = ({ listToRender }: SkeletonCardProps) => {
   return (
     <>
       {skeletons.map((_, index) => (
-        <article
+        <div
           className="product-card__film rounded-2xl shadow-lg shadow-grey/10"
           key={index}
         >
           <div className="w-full h-420 rounded-t-2xl bg-grey/20"></div>
           <div className="p-20">
-            <h2 className=" font-semibold text-xl truncate bg-grey/20 rounded-md h-28"></h2>
-            <p className="mt-10 bg-grey/20 font-medium line-clamp-2 rounded-md h-50"></p>
+            <div className="bg-grey/20 rounded-md h-28"></div>
+            <div className="mt-10 bg-grey/20 rounded-md h-50"></div>
           </div>
-        </article>
+        </div>
       ))}
     </>
   );
--- src/components/ui/SkeletonMoviePage.tsx
+++ src/components/ui/SkeletonMoviePage.tsx
@@ -3,18 +3,17 @@ const SkeletonMoviePage = () => {
     <section className="section-container">
       <div className="relative flex">
         <div className="rounded-2xl opacity-80 h-660 w-440 bg-lightdark" />
-        <div className="flex  flex-col gap-20 ml-40 text-white ">
-          <h1 className="text-3xl rounded-xl font-semibold w-500 h-40 bg-lightdark"></h1>
-          <p className="p-20 bg-lightdark rounded-2xl w-700 h-180"></p>
-          <div className="flex gap-20 items-center w-200 h-40 bg-lightdark rounded-2xl"></div>
-          <div className="flex gap-20 items-center w-200 h-40 bg-lightdark rounded-2xl"></div>
-          <div className="flex gap-20 items-center w-200 h-40 bg-lightdark rounded-2xl"></div>
-          <div className="flex gap-20 items-center w-200 h-40 bg-lightdark rounded-2xl"></div>
-
-          <div className="grid grid-rows-1 grid-cols-3 max-w-700 gap-20 h-160 overflow-hidden">
-            <div className="rounded-2xl shadow-lg shadow-grey/10 w-full h-full bg-lightdark"></div>
-            <div className="rounded-2xl shadow-lg shadow-grey/10 w-full h-full bg-lightdark"></div>
-            <div className="rounded-2xl shadow-lg shadow-grey/10 w-full h-full bg-lightdark"></div>
+        <div className="flex flex-col gap-20 ml-40">
+          <div className="rounded-xl w-500 h-40 bg-lightdark"></div>
+          <div className="p-20 bg-lightdark rounded-2xl w-700 h-180"></div>
+          <div className="flex gap-20">
+            <div className="w-200 h-40 bg-lightdark rounded-2xl"></div>
+            <div className="w-200 h-40 bg-lightdark rounded-2xl"></div>
+          </div>
+          <div className="grid grid-cols-3 gap-20 h-160">
+            <div className="rounded-2xl w-full h-full bg-lightdark"></div>
+            <div className="rounded-2xl w-full h-full bg-lightdark"></div>
+            <div className="rounded-2xl w-full h-full bg-lightdark"></div>
           </div>
         </div>
       </div>
